03ce9aa1a41b7b0c1c043b2256b3e1159d92d595
dalaluf83@gmail.com <dalaluf83@gmail.com@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-11-07 12:47:38 +0000

    I added DashboardActivity.java and dashboard_activity.xml

---
+++ trunk/client/MeeBa/src/com/meeba/google/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.meeba.google;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Padi on 07/11/13.
+ */
+public class DashboardActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard_activity);
+    }
+}
