768b8931a4c3230d8ef7764ce5164630262275ac
eidanspiegel <eidanspiegel@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-11-09 10:16:51 +0000

    Change visiibility of UserFunctions' methods

--- trunk/client/MeeBa/src/com/meeba/google/Database/UserFunctions.java
+++ trunk/client/MeeBa/src/com/meeba/google/Database/UserFunctions.java
@@ -12,35 +12,35 @@ public class UserFunctions {
 
     // TODO: will complete this once server code is ready
 
-    User getUserByEmail(String email) {
+    public static User getUserByEmail(String email) {
         return null;
     }
 
-    User createUser(String email, String name, String phone, String rid) {
+    public static User createUser(String email, String name, String phone, String rid) {
         return null;
     }
 
-    List<Event> getEventsByUser(int uid) {
+    public static List<Event> getEventsByUser(int uid) {
         return null;
     }
 
-    List<User> getUsersByPhones(List<String> phones) {
+    public static List<User> getUsersByPhones(List<String> phones) {
         return null;
     }
 
-    Event createEvent(int uid, String where, String when, List<String> emails) {
+    public static Event createEvent(int uid, String where, String when, List<String> emails) {
         return null;
     }
 
-    void acceptInvite(int uid, int eid) {
+    public static void acceptInvite(int uid, int eid) {
 
     }
 
-    void declineInvite(int uid, int eid) {
+    public static void declineInvite(int uid, int eid) {
 
     }
 
-    List<User> getUsersOfEvent(int eid) {
+    public static List<User> getUsersOfEvent(int eid) {
         return null;
     }
 }
