f9839d854f9305cfbd0127bf1f9848118416b173
eidanspiegel <eidanspiegel@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-11-09 14:21:02 +0000

    Finished server code

--- server/db_functions.php
+++ server/db_functions.php
@@ -8,6 +8,7 @@ class DB_Functions {
     // constructor
     function __construct() {
         require_once 'db_connect.php';
+
         // connecting to database
         $this->db = new DB_Connect();
         $this->db->connect();
@@ -63,7 +64,249 @@ class DB_Functions {
             return false;
         }
     }
-	
+
+    /**
+    * Get user by uid
+    */
+    public function getUserByUid($uid) {
+        $result = mysql_query("SELECT * FROM users WHERE uid = $uid") or die(mysql_error());
+
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            return mysql_fetch_assoc($result);
+        } else {
+            // user not found
+            return false;
+        }
+    }
+
+    /**
+    * Get event by eid
+    */
+    public function getEventByEid($eid) {
+        $result = mysql_query("SELECT * FROM events WHERE eid = $eid") or die(mysql_error());
+
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            return mysql_fetch_assoc($result);
+        } else {
+            // user not found
+            return false;
+        }
+    }
+
+    /**
+    * Get events of user by uid
+    */
+	public function getEventsByUser($uid) {
+        $result = mysql_query("SELECT i.eid, e.created_at FROM `invites` i, `events` e WHERE guest_uid = $uid and i.eid=e.eid UNION ".
+                              "SELECT eid,created_at FROM `events` WHERE host_uid = $uid ORDER BY created_at DESC") or die(mysql_error());
+
+        $events = array();
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            $i = 0;
+            while($row = mysql_fetch_array($result)) {
+                $eid = $row['eid'];
+                
+                $eventdata = mysql_query("SELECT * from `events` WHERE eid = $eid");
+                if(!$eventdata) {
+                    continue;
+                }           
+                
+                $data = mysql_fetch_array($eventdata);
+                $hostuid = $data['host_uid'];            
+                
+                $events[$i]['eid'] = $eid;
+                $events[$i]['host_uid'] = $hostuid;
+                $events[$i]['where'] = $data['where'];
+                $events[$i]['when'] = $data['when'];
+                $events[$i]['created_at'] = $data['created_at'];
+
+                $i = $i + 1;
+            }
+
+            return $events;
+        } else {
+            return false;
+        }    
+    }
+
+    /**
+    * Get an array of users from an array of phones
+    */
+    public function getUsersByPhones($phones) {
+        $phone_numbers = array();
+
+        
+        $phoneList = implode("," , $phones);
+        $sql = 'SELECT * FROM users WHERE phone_number IN ('. $phoneList .')';
+        $result = mysql_query($sql) or die(mysql_error());
+        
+        $no_of_rows = mysql_num_rows($result);
+        if ($no_of_rows > 0) {
+            $users = array();
+
+            $i=0;
+            while($r = mysql_fetch_assoc($result)) {
+                $users[$i] = $r;
+                $i = $i + 1;
+            }
+
+            return $users;
+        }
+        else
+            return false;    
+    }
+
+    /**
+    * Create an event, create and send out invitations to guests via gcm
+    */
+    public function createEvent($host_uid, $where, $when, $uid) {
+        $result = mysql_query("INSERT INTO events(host_uid, `where`, `when`, `created_at`) VALUES('$host_uid', '$where', '$when', NOW())")
+            or die(mysql_error());
+
+        if(!$result) {
+            return false;
+        }
+
+        $eid = mysql_insert_id();
+        $event = mysql_query("SELECT * FROM `events` WHERE eid = $eid");
+        $event = mysql_fetch_assoc($event);
+
+        // Lets get an array of rids
+        $guests = array();
+        $rids = array();
+        foreach($uid as $guest) {
+            $user = $this->getUserByUid($guest);
+
+            array_push($guests, $user);
+            array_push($rids, $user['rid']);
+        }
+
+        // Get the host of the event
+        $host = $this->getUserByUid($host_uid);
+
+        // Create invitations for all the guests
+        $this->createInvite($eid, $guests);
+
+        // Send out invitations to an array of guest rids via gcm
+        $this->sendInvite($host, $rids, $event);
+
+        return $event;
+    }
+
+    /**
+    * Create an invite for an array of guests
+    */
+    private function createInvite($eid, $guests) {
+        foreach ($guests as $guest) {
+            $uid = $guest['uid'];
+            $result = mysql_query("INSERT INTO invites(eid, guest_uid, invite_status) VALUES($eid, $uid, 0)") or die(mysql_error());
+
+            if(!$result) {
+               //$app->getLog()->info("Failed to create invite for user uid=$uid");
+            }
+        }
+    }
+
+    /**
+    * Send an invitation (notification) to an array of "rid"s
+    *
+    */
+    public function sendInvite($host, $rids, $event) {
+        // Message to send      
+        $apiKey = API_KEY;
+        
+        // @Todo
+        // Later we will want to add the list of guest names in the notification
+        $response = $this->sendNotification( 
+            $apiKey, 
+            $rids, //array($registrationId), 
+            array(
+                'tag' => 'invite',
+                'where' => $event['where'],
+                'when' => $event['when'],
+                'sender' => $host['name'],
+                'senderRid' => $host['rid'],
+                'senderUid' => $host['uid'],
+                'eid' => $event['eid']
+                )
+            );
+        
+        return $response;
+    }
+
+    public function respondToInvite($uid, $eid, $status) {
+        // Status can only be 0, 1 or -1
+        if($status != 0 && $status != 1 && $status != -1) {
+            return FALSE;
+        }
+
+        $result = mysql_query("UPDATE invites SET invite_status=$status WHERE eid=$eid AND guest_uid=$uid");
+
+        // Send notification back to the event host ...
+        $event = $this->getEventByEid($eid);
+        $where = $event['where'];
+        $when = $event['when'];
+        $hostUid = $event['host_uid'];
+
+        // Get guest and user arrays
+        $guest = $this->getUserByUid($uid);
+        $host = $this->getUserByUid($hostUid);
+
+        // Get the 'rid' of the host
+        $host_rid = $host['rid'];
+
+        $apiKey = API_KEY;
+        $response = $this->sendNotification(
+                $apiKey,
+                array($host_rid), //array($registrationId),
+                array('tag' => 'inviteResponse',
+                      'eid' => $eid,
+                      'uid' => $uid,
+                      'hostUid' => $hostUid,
+                      'guestName' => $guest['name'],
+                      'when' => $when,
+                      'where' => $where,
+                      'hostName' => $host['name'],
+                      'status' => $status,
+                      )
+                ); 
+
+        return $response;
+    }
+
+    /**
+     * The following function will send a GCM notification using curl.
+     * 
+     * @param $apiKey       [string] The Browser API key string for your GCM account
+     * @param $registrationIdsArray [array]  An array of registration ids to send this notification to
+     * @param $messageData      [array]  An named array of data to send as the notification payload
+     */
+    private function sendNotification( $apiKey, $registrationIdsArray, $messageData )
+    {   
+        $headers = array("Content-Type:" . "application/json", "Authorization:" . "key=" . $apiKey);
+        $data = array(
+            'data' => $messageData,
+            'registration_ids' => $registrationIdsArray
+        );
+     
+        $ch = curl_init();
+     
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers ); 
+        curl_setopt( $ch, CURLOPT_URL, "https://android.googleapis.com/gcm/send" );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0 );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0 );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($data) );
+     
+        $response = curl_exec($ch);
+        curl_close($ch);
+     
+        return $response;
+    }
+
 }
  
 ?>
--- server/index.php
+++ server/index.php
@@ -24,46 +24,7 @@ $writer = $log->getWriter();
 
 // include db handler
 require_once 'db_functions.php';
-$db = new DB_Functions();
-
-$app->post('/login', function() use ($app, $db) {
-	$email = $_POST['email'];
-    $name = $_POST['name'];
-    $rid = $_POST['rid'];
-    $phone = $_POST['phone'];
-    // TODO: Add google access token
-		
-	$user = $db->getUserByEmail($email); // check for user by email
-	$user2 = $db->getUserByPhone($phone); // check for user by phone
-	if ($user != false || $user2 != false) { 		
- 		echo json_encode(array(
-			'success' => 1,
-			'error' => null,
-			'new' => 0,
-			'user' => (($user!=false)?$user:$user2)
-		));
-
-		return;
-	}
-
-	// Otherwise store a new user
-	$user = $db->createUser($email, $name, $rid, $phone);
-	if(!$user) {
-		echo json_encode(array(
-			'success' => 0,
-			'error' => "Couldn't create user",
-			'new' => 0,
-			'user' => null
-			));
-	}
-
-	echo json_encode(array(
-		'success' => 1,
-		'error' => null,
-		'new' => 1,
-		'user' => $user
-	));
-});
+$db = new DB_Functions($app);
 
 $app->get('/getUserByPhone/:phone', function($phone) use ($app, $db) {
 	$user = $db->getUserByPhone($phone);
@@ -81,6 +42,163 @@ $app->get('/getUserByPhone/:phone', function($phone) use ($app, $db) {
 			'error' => null,
 			'user' => $user
 			));
+});
+
+$app->get('/getUserByEmail/:email', function($email) use ($app, $db) {
+	$user = $db->getUserByEmail($email);
+	if(!$user) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Couldn't find user with email $email",
+			'user' => null
+			));	
+		return;
+	}
+
+	echo json_encode(array(
+			'success' => 1,
+			'error' => null,
+			'user' => $user
+			));	
+});
+
+$app->post('/createUser', function() use ($app, $db) {
+	$email = $_POST['email'];
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$rid = $_POST['rid'];
+
+	// First we check if a user exists by the same email or phone
+	$user = $db->getUserByEmail($email); // check for user by email
+	$user2 = $db->getUserByPhone($phone); // check for user by phone
+	if ($user != false || $user2 != false) { 		
+ 		echo json_encode(array(
+			'success' => 0,
+			'error' => "A user already exists with the same " . (($user!=false)?"email":"phone"),
+			'user' => null,
+		));
+
+		return;
+	}
+
+	// Otherwise store a new user
+	$user = $db->createUser($email, $name, $rid, $phone);
+	if(!$user) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Couldn't create user",
+			'user' => null
+			));
+	}
+
+	echo json_encode(array(
+		'success' => 1,
+		'error' => null,
+		'user' => $user
+	));
+});
+
+$app->get('/getEventsByUser/:uid', function($uid) use ($app, $db) {
+	$events = $db->getEventsByUser($uid);
+
+	if(!$events) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Failed to get events of user $uid",
+			'events' => null,
+		));
+
+		return;
+	}
+
+	echo json_encode(array(
+			'success' => 1,
+			'error' => null,
+			'events' => $events,
+		));
+});
+
+$app->post('/getUsersByPhones', function() use ($app, $db) {
+	$phones = $_POST['phones'];
+	$phones = json_decode($phones); // decode the json array
+	$app->getLog()->info($phones);
+	$users = $db->getUsersByPhones($phones);
+
+	if(!$users) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Failed to get users from phone array",
+			'users' => null,
+		));
+
+		return;
+	}
+
+	echo json_encode(array(
+			'success' => 1,
+			'error' => null,
+			'users' => $users,
+		));
+
+});
+
+$app->post('/createEvent', function() use ($app, $db) {
+	$host_uid = $_POST['host_uid'];
+	$where = $_POST['where'];
+	$when = $_POST['when'];
+	$uid = $_POST['uid']; // array of guest uids
+
+	$uid = json_decode($uid); // decode the json array
+
+	// if the host is also invited then remove him from the guest array of uids
+	// shouldn't happen normally
+	$key = array_search($host_uid, $uid);
+	if($key != false) { 
+		unset($uid[$key]);
+		$uid = array_values($uid);
+	}
+
+	// Create the event and sent out invites
+	$event = $db->createEvent($host_uid, $where, $when, $uid);
+
+	if(!$event) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Failed to create the event",
+			'event' => null,
+		));
+
+		return;
+	}
+
+	echo json_encode(array(
+			'success' => 1,
+			'error' => null,
+			'event' => $event,
+		));	
+	
+});
+
+$app->post('/respondToInvite', function() use ($app, $db) {
+	$uid = $_POST['uid'];
+	$eid = $_POST['eid'];
+	$status = $_POST['status'];
+
+	$result = $db->respondToInvite($uid, $eid, $status);
+
+	if(!$result) {
+		echo json_encode(array(
+			'success' => 0,
+			'error' => "Failed to respond to the invitation",
+		));
+
+		return;
+	}
+
+	echo json_encode(array(
+		'success' => 1,
+		'error' => null
+	));
 });
 
 $app->run();
