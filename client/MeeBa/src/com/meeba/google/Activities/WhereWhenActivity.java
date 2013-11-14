683c68f1c757ebc9a3802975729f3164cf8a4142
malkaor@gmail.com <malkaor@gmail.com@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-11-14 20:18:26 +0000

    2 new activities

---
+++ client/MeeBa/src/com/meeba/google/Activities/WhereWhenActivity.java
@@ -0,0 +1,55 @@
+package com.meeba.google.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.meeba.google.R;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by or malka on 09/11/13.
+ */
+public class WhereWhenActivity extends Activity{
+
+    EditText editWhere;
+    EditText editWhen;
+    Button next;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.where_when_activity);
+        editWhen = (EditText) findViewById(R.id.whenTxtUser);
+        editWhere = (EditText) findViewById(R.id.whereTxtUser);
+        next = (Button) findViewById(R.id.NextBtn);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String where = editWhere.getText().toString();
+                final String when = editWhen.getText().toString();
+                if(TextUtils.isEmpty(where) || TextUtils.isEmpty(when)) {
+                    Toast.makeText(getApplicationContext(),"You must input a location and a time" ,Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                //when we click on the button it will bring us to the contacts activity
+                Intent i = new Intent(getApplicationContext(),
+                        ContactsActivity.class);
+                //build this so i can use user's input in the post activity
+                Bundle bundle = new Bundle();
+                bundle.putString("when", when);
+                bundle.putString("where", where);
+                i.putExtras(bundle);
+                startActivity(i);
+
+            }
+        });
+    }
+
+
+
+}
+
+
