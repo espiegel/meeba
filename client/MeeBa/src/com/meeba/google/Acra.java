1ecfe7b202e48c946d955af9c95d9e5042ac92d3
eidanspiegel <eidanspiegel@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-12-09 23:24:46 +0000

    updated acra to use a dialog instead of a toast

--- client/MeeBa/src/com/meeba/google/Acra.java
+++ client/MeeBa/src/com/meeba/google/Acra.java
@@ -7,12 +7,15 @@ import org.acra.annotation.*;
 /**
  * Created by Eidan on 12/6/13.
  */
-@ReportsCrashes(
-        formKey = "", // will not be used
+@ReportsCrashes(formKey = "", // will not be used
         mailTo = "meeba-dev@googlegroups.com",
-        mode = ReportingInteractionMode.TOAST,
-        resToastText = R.string.crash_toast_text
-)
+        mode = ReportingInteractionMode.DIALOG,
+        resDialogText = R.string.crash_dialog_text,
+        resDialogIcon = android.R.drawable.ic_dialog_info, //optional. default is a warning sign
+        resDialogTitle = R.string.crash_dialog_title, // optional. default is your application name
+        resDialogCommentPrompt = R.string.crash_dialog_comment_prompt, // optional. when defined, adds a user text field input with this text resource as a label
+        resDialogOkToast = R.string.crash_dialog_ok_toast, // optional. displays a Toast message when the user accepts to send a report.
+        resToastText = R.string.acra)
 public class Acra extends Application {
     @Override
     public void onCreate() {
