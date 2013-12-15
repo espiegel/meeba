59ee1c2470c2c4b8ea4a1992e73f695cfc3c85f0
eidanspiegel <eidanspiegel@1a69d080-33a6-a178-0004-a98bc7fb1081>
2013-12-15 17:26:20 +0000

    phone numbers are only numeric now

--- client/MeeBa/src/com/meeba/google/util/Utils.java
+++ client/MeeBa/src/com/meeba/google/util/Utils.java
@@ -112,7 +112,8 @@ public class Utils {
                     String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
                     // Filter out all the "-"'s and "*"'s from the phone number
-                    phoneNumber = phoneNumber.replaceAll("\\+972", "0").replaceAll(" ", "").replaceAll("-", "").replaceAll("\\*", "").replaceAll("[)(]]", "");
+                    phoneNumber = phoneNumber.replaceAll("[^0-9]","");
+                    //phoneNumber = phoneNumber.replaceAll("\\+972", "0").replaceAll(" ", "").replaceAll("-", "").replaceAll("\\*", "").replaceAll("[)(]]", "");
 
                     phonesMap.put(phoneNumber, contact);
                 }
