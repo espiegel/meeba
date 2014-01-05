62275b9c41f4766106c31c6f368c06e34138ffdb
dalaluf83@gmail.com <dalaluf83@gmail.com@1a69d080-33a6-a178-0004-a98bc7fb1081>
2014-01-05 14:30:22 +0000

    filter invites "like" in whatsapp :)

--- client/MeeBa/src/com/meeba/google/adapters/ContactsArrayAdapter.java
+++ client/MeeBa/src/com/meeba/google/adapters/ContactsArrayAdapter.java
@@ -6,8 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,6 +23,7 @@ import java.util.List;
 public class ContactsArrayAdapter extends ArrayAdapter<User> implements Filterable {
 
     private List<User> list;
+    //private List<User> filterList;
     private Activity mContext;
     private ImageLoader mImageLoader;
 
@@ -32,16 +31,24 @@ public class ContactsArrayAdapter extends ArrayAdapter<User> implements Filterab
         super(context, R.layout.contactlistlayout, list);
         this.mContext = context;
         this.list = list;
+        //this.filterList = list;
     }
 
     static class ViewHolder {
         protected TextView text;
-        protected CheckBox checkbox;
+        protected ImageView deleteMe;
+       // protected CheckBox checkbox;
         protected ImageView imageView;
+    }
+
+    public int getCount() {
+        return list.size();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Utils.LOGD("position ="+position);
+        Utils.LOGD("filterResults="+list);
         View view;
         if (convertView == null) {
             LayoutInflater inflator = mContext.getLayoutInflater();
@@ -49,8 +56,18 @@ public class ContactsArrayAdapter extends ArrayAdapter<User> implements Filterab
             final ViewHolder viewHolder = new ViewHolder();
             viewHolder.text = (TextView) view.findViewById(R.id.label);
             viewHolder.imageView = (ImageView) view.findViewById(R.id.personPicture);
-            viewHolder.checkbox = (CheckBox) view.findViewById(R.id.check);
-            viewHolder.checkbox
+            viewHolder.deleteMe=(ImageView) view.findViewById(R.id.deleteMe);
+            /*viewHolder.deleteMe.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    User element = (User) viewHolder.deleteMe
+                            .getTag();
+                    ContactsActivity.addToInvitList(element);
+                }
+            });*/
+
+           /* viewHolder.checkbox = (CheckBox) view.findViewById(R.id.check);*/
+           /*viewHolder.checkbox
                     .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                         public void onCheckedChanged(CompoundButton buttonView,
                                                      boolean isChecked) {
@@ -60,23 +77,23 @@ public class ContactsArrayAdapter extends ArrayAdapter<User> implements Filterab
                             Utils.LOGD("changed selection to "+buttonView.isChecked());
                             Utils.LOGD("element= " + element.isSelected());
                         }
-                    });
+                    });*/
             view.setTag(viewHolder);
-            viewHolder.checkbox.setTag(list.get(position));
+           /* viewHolder.checkbox.setTag(list.get(position));*/
         } else {
             view = convertView;
-            ((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));
+            /*((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));*/
         }
         final ViewHolder holder = (ViewHolder) view.getTag();
 
         User user = list.get(position);
         // For now just show the name
         String name = user.getName();
-        boolean check = user.isSelected();
+        //boolean check = user.isSelected();
         String picture_url = user.getPicture_url();
 
         holder.text.setText(name);
-        holder.checkbox.setChecked(check);
+        /*holder.checkbox.setChecked(check);*/
         Utils.LOGD("getView: position="+position+", user="+user);
 
 
@@ -92,7 +109,43 @@ public class ContactsArrayAdapter extends ArrayAdapter<User> implements Filterab
         return view;
     }
 
-    public List<User> getList() {
-        return list;
-    }
+    public List<User> getList() { return list; }
+
+   /* @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                // We implement here the filter logic
+                if (constraint == null || constraint.length() == 0) {
+                    // No filter implemented we return all the list
+                    results.values = list;
+                    results.count = list.size();
+                }
+                else {
+                    // We perform filtering operation
+                    ArrayList<User> filterData = new ArrayList<User>();
+
+                    for (User p : list) {
+                        if (p.getName().toLowerCase().contains(constraint.toString().toLowerCase()))
+                            filterData.add(p);
+                    }
+
+                    results.values = filterData;
+                    results.count = filterData.size();
+
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint,
+                                          FilterResults results) {
+                filterList = (List<User>) results.values;
+                notifyDataSetChanged();
+            }
+        };
+    }*/
 }
+
---
+++ client/MeeBa/src/com/meeba/google/adapters/ContactsAutoCompleteAdapter.java
@@ -0,0 +1,150 @@
+package com.meeba.google.adapters;
+
+/**
+ * Created by Padi on 01/01/14.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.meeba.google.R;
+import com.meeba.google.activities.ContactsActivity;
+import com.meeba.google.objects.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Eidan on 12/18/13.
+ */
+public class ContactsAutoCompleteAdapter extends ArrayAdapter<User> implements Filterable {
+    private final SearchAutoComplete mCallback;
+    private List<User> list;
+    private List<User> filterList;
+    private final Context mContext;
+
+    public ContactsAutoCompleteAdapter(Context context, int layoutResourceId, int textViewResourceId , List<User> list, SearchAutoComplete callback) {
+        super(context, layoutResourceId, textViewResourceId);
+
+        this.list = list;
+        this.filterList = list;
+        mContext = context;
+        mCallback = callback;
+    }
+
+    @Override
+    public int getCount() {
+        return filterList.size();
+    }
+
+    @Override
+    public User getItem(int index) {
+        return filterList.get(index);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.dropdown_autocomplete, parent, false);
+        }
+
+        final TextView txtView = (TextView) convertView.findViewById(R.id.txtViewSearch);
+
+        if(position >= filterList.size()) {
+            return convertView;
+        }
+
+        User currentUser = filterList.get(position);
+
+        txtView.setText(currentUser.getName());
+
+        txtView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view == null) {
+                    return;
+                }
+                mCallback.autoCompleteItemClicked("");
+                ContactsActivity.addToInvitList(filterList.get(position));
+            }
+        });
+
+        RelativeLayout layout = (RelativeLayout) convertView.findViewById(R.id.layoutAutoComplete);
+
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view == null) {
+                    return;
+                }
+
+                for (int itemPos = 0; itemPos < ((ViewGroup)view).getChildCount(); itemPos++) {
+                    View child = ((ViewGroup)view).getChildAt(itemPos);
+                    if (child instanceof TextView) {
+                        TextView textView = (TextView) child; //Found it!
+
+                        mCallback.autoCompleteItemClicked(textView.getText().toString());
+
+                        break;
+                    }
+                }
+            }
+        });
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(final CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                // We implement here the filter logic
+                if (constraint == null || constraint.length() == 0) {
+                    // No filter implemented we return all the list
+                    results.values = list;
+                    results.count = list.size();
+                }
+                else {
+                    // We perform filtering operation
+                    ArrayList<User> filterData = new ArrayList<User>();
+
+                    for (User p : list) {
+                        if (p.getName().toLowerCase().startsWith(constraint.toString().toLowerCase()))
+                            filterData.add(p);
+                    }
+
+                    results.values = filterData;
+                    results.count = filterData.size();
+
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    filterList = (List<User>) results.values;
+                    notifyDataSetChanged();
+                } else {
+                    filterList = (List<User>) results.values;
+                    notifyDataSetInvalidated();
+                }
+            }
+        };
+        return filter;
+    }
+
+    public interface SearchAutoComplete {
+        public void autoCompleteItemClicked(String query);
+    }
+}
